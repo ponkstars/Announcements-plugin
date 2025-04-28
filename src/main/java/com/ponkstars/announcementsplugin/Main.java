154631e1c0bbb452e5b8025e8b9819b19ac7deff
ponkstars <159983729+ponkstars@users.noreply.github.com>
2025-04-27 22:18:21 -0400

    Update Main.java

--- src/main/java/com/ponkstars/announcementsplugin/Main.java
+++ src/main/java/com/ponkstars/announcementsplugin/Main.java
@@ -1,1 +1,51 @@
+package com.ponkstars.announcementsplugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("AnnouncementsPlugin has been enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("AnnouncementsPlugin has been disabled.");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("announce")) {
+            // Ensure the sender is a player (or console can send an announcement)
+            if (sender instanceof Player || sender.hasPermission("announce.use")) {
+                if (args.length > 0) {
+                    StringBuilder message = new StringBuilder();
+                    for (String arg : args) {
+                        message.append(arg).append(" ");
+                    }
+
+                    // Send the announcement to all players on the server
+                    sendAnnouncement(message.toString().trim());
+                } else {
+                    sender.sendMessage("Usage: /announce <message>");
+                }
+                return true;
+            } else {
+                sender.sendMessage("You don't have permission to use this command.");
+            }
+        }
+        return false;
+    }
+
+    // Method to send the announcement to all players
+    private void sendAnnouncement(String message) {
+        for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+            player.sendActionBar(message);
+        }
+    }
+}
